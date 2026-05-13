31c40508071fcdf5c28bc5d5b55f1a924cd9767e
msilich <msilich@users.noreply.github.com>
2026-05-13 19:34:55 +0200

    Add optional hcloud data volumes

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # hf-tf-module-hcloud
+
+Terraform module for HobbyFarm VMs on Hetzner Cloud.
+
+## Optional extra volumes
+
+The module can attach additional raw Hetzner Cloud volumes to the server. They
+are not formatted or mounted by Terraform, so labs can use them as raw block
+devices, for example as Ceph OSD disks.
+
+Variables:
+
+- `extra_volume_count`: number of additional volumes, default `0`
+- `extra_volume_size`: size of each additional volume in GB, default `10`
+- `extra_volume_labels`: optional comma-separated labels, for example `lab=ceph,role=osd`
+
+Example HobbyFarm template config:
+
+```yaml
+config_map:
+  server_type: cx23
+  ssh_username: root
+  extra_volume_count: "2"
+  extra_volume_size: "10"
+  extra_volume_labels: "lab=ceph,role=osd"
+```
--- main.tf
+++ main.tf
@@ -30,17 +30,35 @@ variable "poll_function" {
   default     = "exponential"
   description = "Configures the type of function to be used during the polling. Valid values are constant and exponential"
 }
+variable "extra_volume_count" {
+  type        = string
+  default     = "0"
+  description = "Number of additional Hetzner Cloud volumes to attach to the server"
+}
+variable "extra_volume_size" {
+  type        = string
+  default     = "10"
+  description = "Size in GB for each additional Hetzner Cloud volume"
+}
+variable "extra_volume_labels" {
+  type        = string
+  default     = ""
+  description = "Optional labels for additional volumes in 'key1=value1,key2=value2' format"
+}
 
 
 locals {
-  labels_map = length(var.labels) > 0 ? { for pair in split(",", var.labels) : split("=", pair)[0] => split("=", pair)[1] } : {}
-  firewall_ids_list = length(var.firewall_ids) > 0 ? split(",", var.firewall_ids) : []
+  labels_map              = length(var.labels) > 0 ? { for pair in split(",", var.labels) : split("=", pair)[0] => split("=", pair)[1] } : {}
+  firewall_ids_list       = length(var.firewall_ids) > 0 ? split(",", var.firewall_ids) : []
+  extra_volume_count      = tonumber(var.extra_volume_count)
+  extra_volume_size       = tonumber(var.extra_volume_size)
+  extra_volume_labels_map = length(var.extra_volume_labels) > 0 ? { for pair in split(",", var.extra_volume_labels) : split("=", pair)[0] => split("=", pair)[1] } : {}
 }
 
 terraform {
   required_providers {
     hcloud = {
-      source = "hetznercloud/hcloud"
+      source  = "hetznercloud/hcloud"
       version = "~> 1.45.0"
     }
   }
@@ -49,27 +67,27 @@ terraform {
 
 #Configure the Hetzner Cloud Provider
 provider "hcloud" {
-  token = var.hcloud_token
+  token         = var.hcloud_token
   poll_interval = var.poll_interval
   poll_function = var.poll_function
 }
 
 # Create a new SSH key
 resource "hcloud_ssh_key" "key" {
-  name = "${var.name}-key"
+  name       = "${var.name}-key"
   public_key = var.public_key
 }
 
 # Create a new server running debian
 resource "hcloud_server" "node1" {
-  name = var.name
-  image = var.image
-  location = var.location
+  name        = var.name
+  image       = var.image
+  location    = var.location
   server_type = var.server_type
-  ssh_keys = ["${var.name}-key"]
-  user_data = var.cloud-config
+  ssh_keys    = ["${var.name}-key"]
+  user_data   = var.cloud-config
 
-  labels = local.labels_map
+  labels       = local.labels_map
   firewall_ids = length(local.firewall_ids_list) > 0 ? local.firewall_ids_list : null
 
   dynamic "network" {
@@ -78,6 +96,23 @@ resource "hcloud_server" "node1" {
       network_id = network.value
     }
   }
+}
+
+resource "hcloud_volume" "extra" {
+  count = local.extra_volume_count
+
+  name     = "${var.name}-data-${count.index + 1}"
+  size     = local.extra_volume_size
+  location = var.location
+  labels   = local.extra_volume_labels_map
+}
+
+resource "hcloud_volume_attachment" "extra" {
+  count = local.extra_volume_count
+
+  volume_id = hcloud_volume.extra[count.index].id
+  server_id = hcloud_server.node1.id
+  automount = false
 }
 
 output "private_ip" {
@@ -94,4 +129,12 @@ output "public_ip" {
 
 output "hostname" {
   value = hcloud_server.node1.name
+}
+
+output "extra_volume_ids" {
+  value = join(",", [for volume in hcloud_volume.extra : tostring(volume.id)])
+}
+
+output "extra_volume_names" {
+  value = join(",", [for volume in hcloud_volume.extra : volume.name])
 }
